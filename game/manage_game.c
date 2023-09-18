66c20dac461535e63fc7c244421aa2df1f257320
amelmnd <amelmnd.dev@gmail.com>
2023-09-18 19:27:44 +0200

    so_long : feat print img

--- game/manage_game.c
+++ game/manage_game.c
@@ -6,24 +6,61 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 17:44:11 by amennad           #+#    #+#             */
-/*   Updated: 2023/09/18 18:32:41 by amennad          ###   ########.fr       */
+/*   Updated: 2023/09/18 19:19:33 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../mlx/mlx.h"
 #include "so_long.h"
+#include <stdio.h>
+
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	void	*img;
+// 	int		img_width;
+// 	int		img_height;
+
+// 	mlx = mlx_init();
+// 	win = mlx_new_window(mlx, 500, 500, "my_mlx");
+// 	if (!win)
+// 		printf("Error1\n");
+// 	img = mlx_xpm_file_to_image(mlx, "./textures/wall_n.xpm", &img_width,
+// 			&img_height);
+// 	if (!img)
+// 		printf("Error2\n");
+// 	mlx_put_image_to_window(mlx, win, img, 50, 50);
+// 	mlx_loop(mlx);
+// 	return (0);
+// }
 
 void	manage_game(char **map, int nb_line, int len_line)
 {
 	t_window window;
-	t_img image;
+	// t_img image;
+	int img_width;
+	int img_height;
+	void *img;
 
 	window = new_game((len_line * 100), (nb_line * 100), "so_long");
 	if (!window.mlx_ptr || !window.win_ptr)
 		ft_exit_free(1, map, NULL, "Error\nmlx_init() failed\n");
-	image = new_img((len_line * 100), (nb_line * 100), window);
-	put_pixel_img(image, 250, 100, 0x00FFFFFF);
-	mlx_put_image_to_window(image.win.mlx_ptr, image.win.win_ptr, image.img_ptr,
-			0, 0);
+
+	//AFFICHER DES PIXELS
+	// image = new_img((len_line * 100), (nb_line * 100), window);
+	// put_pixel_img(image, 250, 100, 0x00FFFFFF);
+	// mlx_put_image_to_window(image.win.mlx_ptr, image.win.win_ptr,
+	// image.img_ptr,0, 0);
+
+	//AFFICHER UNE IMAGE
+	img = mlx_xpm_file_to_image(window.mlx_ptr, "./game/flower.xpm", &img_width,
+			&img_height);
+	if (!img)
+		printf("Error2\n");
+	mlx_put_image_to_window(window.mlx_ptr, window.win_ptr, img, ((len_line
+					* 100) / 3), ((nb_line * 100) / 2));
+	// mlx_loop(mlx);
 	mlx_hook(window.win_ptr, 2, 0, ft_keyboard_event, &window);
 	mlx_hook(window.win_ptr, 17, 0, ft_mouse_event, &window);
 	mlx_loop(window.mlx_ptr);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:25:04 by amennad           #+#    #+#             */
-/*   Updated: 2023/09/18 18:32:58 by amennad          ###   ########.fr       */
+/*   Updated: 2023/09/18 18:50:50 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,26 @@ int	main(int ac, char **av)
 	manage_game(map, nb_line, len_line);
 	return (0);
 }
+// #include "./mlx/mlx.h"
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	void	*img;
+// 	int		img_width;
+// 	int		img_height;
+
+// 	mlx = mlx_init();
+// 	win = mlx_new_window(mlx, 500, 500, "my_mlx");
+// 	if (!win)
+// 		printf("Error1\n");
+// 	img = mlx_xpm_file_to_image(mlx, "./textures/wall_n.xpm", &img_width,
+// 			&img_height);
+// 	if (!img)
+// 		printf("Error2\n");
+// 	mlx_put_image_to_window(mlx, win, img, 50, 50);
+// 	mlx_loop(mlx);
+// 	return (0);
+// }
